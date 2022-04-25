3806168456abac3cd6d53d016bcccfd3cfdd3c1d
Rodrigo A. Souza <rodrigo_asouza@outlook.com>
2022-04-25 00:45:00 -0300

    Creational - Singleton

---
+++ Design.Patterns.Creational/Singleton/Singleton.cs
@@ -0,0 +1,15 @@
+﻿namespace Design.Patterns.Creational.Singleton
+{
+    public sealed class Singleton
+    {
+        private Singleton() { }
+
+        private static Singleton _Instance = new();
+
+        public static Singleton Get =>
+            _Instance;
+
+        public void Message() =>
+            Console.WriteLine(ToString());
+    }
+}
